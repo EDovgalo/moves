3cbdb0ff3815da34b63476840e64e4d624a214d1
EDovgalo <edovgalo@gmail.com>
2020-07-31 23:27:37 +0300

    refactoring, add resolver

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,19 +1,19 @@
-const path = require("path");
-const webpack = require("webpack");
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+const path = require('path');
+const webpack = require('webpack');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
 module.exports = (env) =>
-  merge(common, {
-    devServer: {
-      contentBase: path.join(__dirname, "prod"),
-      compress: true,
-      open: true,
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        "process.env.APP_VERSION": JSON.stringify(env.APP_VERSION),
-        "process.env.NODE_ENV": JSON.stringify("PROD"),
-      }),
-    ],
-  });
+    merge(common, {
+        devServer: {
+            contentBase: path.join(__dirname, 'prod'),
+            compress: true,
+            open: true,
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                'process.env.APP_VERSION': JSON.stringify(env.APP_VERSION),
+                'process.env.NODE_ENV': JSON.stringify('PROD'),
+            }),
+        ],
+    });
